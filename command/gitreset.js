29ddc674cb57912f8319884fea3a0d282db2eb22
wenjiangf <f_wenjiang@163.com>
2022-03-29 17:01:03 +0800

    修改gitreset,解决未git

--- command/gitreset.js
+++ command/gitreset.js
@@ -1,29 +1,22 @@
 import chalk from 'chalk'
 import child_process from 'child_process'
 
-const { spawn } = child_process
+const { exec } = child_process
 
 export default function (program) {
   program
     .command('gitreset')
     .description('执行回退到上一个版本')
-    .action(() => {
+    .action(async () => {
       try {
-        const gitreset = spawn('git reset', ['--hard', 'HEAD'], {
-          shell: true,
-        })
-        gitreset.stdout.on('data', (data) => {
-          console.log(chalk.blue(`${data}`))
-        })
-        gitreset.stderr.on('data', (err) => {
-          console.log('stderr: ', chalk.red(err))
-        })
-        gitreset.on('error', (err) => {
+        await exec('git add .')
+        const { error, stdout } = await exec('git reset --hard HEAD')
+        if (error) {
           console.log('error: ', chalk.red(err))
-        })
-        gitreset.on('close', (code) => {
-          console.log(`child process exited with code ${code}`)
-        })
+        }
+        if (stdout) {
+          console.log(chalk.blue(`${data}`))
+        }
       } catch (err) {
         console.log(chalk.red(`err: ${err}`))
       }
