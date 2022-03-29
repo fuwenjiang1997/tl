7457ecf4d0407c97b222529f8b220299213991c1
wenjiangf <f_wenjiang@163.com>
2022-03-29 16:52:54 +0800

    默认提交

---
+++ command/gitreset.js
@@ -0,0 +1,31 @@
+import chalk from 'chalk'
+import child_process from 'child_process'
+
+const { spawn } = child_process
+
+export default function (program) {
+  program
+    .command('gitreset')
+    .description('执行回退到上一个版本')
+    .action(() => {
+      try {
+        const gitreset = spawn('git reset', ['HEAD^'], {
+          shell: true,
+        })
+        gitreset.stdout.on('data', (data) => {
+          console.log(chalk.blue(`${data}`))
+        })
+        gitreset.stderr.on('data', (err) => {
+          console.log('stderr: ', chalk.red(err))
+        })
+        gitreset.on('error', (err) => {
+          console.log('error: ', chalk.red(err))
+        })
+        gitreset.on('close', (code) => {
+          console.log(`child process exited with code ${code}`)
+        })
+      } catch (err) {
+        console.log(chalk.red(`err: ${err}`))
+      }
+    })
+}
