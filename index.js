b9a4b64a9ebf471c8ea5df0af083b33347fc4cd6
wenjiangf <f_wenjiang@163.com>
2022-03-31 09:09:45 +0800

    修复动态导入错误的问题

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 import { Command } from 'commander'
 import { readFile } from 'fs/promises'
+import fs from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
 const program = new Command()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const packageJson = JSON.parse(
   await readFile(new URL('./package.json', import.meta.url))
@@ -12,29 +16,21 @@ const packageJson = JSON.parse(
 program.version(packageJson.version, '-v, --version', 'cli的最新版本')
 
 try {
-  const commandFiles = [
-    'createGotplhtml',
-    'createProject',
-    'gitNewBranch',
-    'gitPush',
-    'gitreset',
-    'gitStash',
-    'randomCharts',
-  ]
+  // 自动导入添加command
+  const commandFiles = await fs.promises.readdir(resolve(__dirname, 'command'))
   for (let i = 0; i < commandFiles.length; i++) {
     const fileName = commandFiles[i]
-    let model = await import(`./command/${fileName}.js`)
+    let model = await import(`./command/${fileName}`)
     if (typeof model?.default === 'function') {
       model.default(program)
     }
   }
 
   // 自动导入添加options
-  // await fs.promises.readdir(path.join('options'))
-  const optionFiles = ['lsAll']
+  const optionFiles = await fs.promises.readdir(resolve(__dirname, 'options'))
   for (let i = 0; i < optionFiles.length; i++) {
     const fileName = optionFiles[i]
-    let model = await import(`./options/${fileName}.js`)
+    let model = await import(`./options/${fileName}`)
     model.default(program)
   }
 } catch (err) {
