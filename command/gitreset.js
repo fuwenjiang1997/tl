62903f5b2f5ee104d2a3bb95d2460f30ab772a6c
Fuwenjiang <fuwenjiang@FuwenjiangdeMacBook-Pro.local>
2023-11-21 20:02:49 +0800

    默认提交

--- command/gitreset.js
+++ command/gitreset.js
@@ -11,7 +11,7 @@ export default function (program) {
     .description('执行回退到上一个版本')
     .action(async () => {
       try {
-        await exec('git add .') // 解决未add的情况下不能reset
+        await exec('git checkout .') // 解决未add的情况下不能reset
         handleExeRes(await exec('git reset --hard HEAD'))
       } catch (err) {
         console.log(chalk.red(`err: ${err}`))
